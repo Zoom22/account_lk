99e68a51453eb8d9d2a7a0277fe0edad5c559805
Sergey Inozentsev <is.zoom@yandex.ru>
2021-08-20 09:15:50 +0100

    transaction page layout

---
+++ public/js/app.js
@@ -0,0 +1,32 @@
+$.datepicker.regional['ru'] = {
+    closeText: 'Закрыть',
+    prevText: 'Предыдущий',
+    nextText: 'Следующий',
+    currentText: 'Сегодня',
+    monthNames: ['Январь','Февраль','Март','Апрель','Май','Июнь','Июль','Август','Сентябрь','Октябрь','Ноябрь','Декабрь'],
+    monthNamesShort: ['Янв','Фев','Мар','Апр','Май','Июн','Июл','Авг','Сен','Окт','Ноя','Дек'],
+    dayNames: ['воскресенье','понедельник','вторник','среда','четверг','пятница','суббота'],
+    dayNamesShort: ['вск','пнд','втр','срд','чтв','птн','сбт'],
+    dayNamesMin: ['Вс','Пн','Вт','Ср','Чт','Пт','Сб'],
+    weekHeader: 'Не',
+    dateFormat: 'dd.mm.yy',
+    firstDay: 1,
+    isRTL: false,
+    showMonthAfterYear: false,
+    yearSuffix: ''
+};
+$.datepicker.setDefaults($.datepicker.regional['ru']);
+
+$(function() {
+    var dates = $( "#from, #to" ).datepicker({
+        onSelect: function( selectedDate ) {
+            var option = this.id == "from" ? "minDate" : "maxDate",
+                instance = $( this ).data( "datepicker" ),
+                date = $.datepicker.parseDate(
+                    instance.settings.dateFormat ||
+                    $.datepicker._defaults.dateFormat,
+                    selectedDate, instance.settings );
+            dates.not( this ).datepicker( "option", option, date );
+        }
+    });
+});
--- src/Controller/ServicesController.php
+++ src/Controller/ServicesController.php
@@ -20,4 +20,51 @@ class ServicesController extends AbstractController
             'year' => date('Y'),
         ]);
     }
+
+    /**
+     * @Route("/transactions")
+     *
+     */
+    public function transactions()
+    {
+        $transactions = [
+            [
+                'id' => 1,
+                'date' => '30.06.2021',
+                'service' => 'Домофон',
+                'period' => 'июль',
+                'price' => 150,
+                'balance' => 500,
+            ],
+            [
+                'id' => 2,
+                'date' => '30.06.2021',
+                'service' => 'Лифт',
+                'period' => 'июль',
+                'price' => 160,
+                'balance' => 340,
+            ],
+            [
+                'id' => 3,
+                'date' => '31.07.2021',
+                'service' => 'Домофон',
+                'period' => 'август',
+                'price' => 150,
+                'balance' => 500,
+            ],
+            [
+                'id' => 4,
+                'date' => '31.07.2021',
+                'service' => 'Лифт',
+                'period' => 'август',
+                'price' => 160,
+                'balance' => 340,
+            ],
+        ];
+        return $this->render("transactions.html.twig", [
+            'title' => 'История операций',
+            'year' => date('Y'),
+            'transactions' => $transactions,
+        ]);
+    }
 }
